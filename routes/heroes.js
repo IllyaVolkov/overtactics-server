5a5321ff40436055af803b8e6a3be7410f50b6fa
IllyaVolkov <volkov18598>
2018-12-08 02:36:24 +0200

    added loading of relative skills

--- routes/heroes.js
+++ routes/heroes.js
@@ -3,15 +3,20 @@ var router = express.Router();
 
 var loki = require('lokijs');
 var db = new loki('mock.json', {autosave: true, autosaveInterval: 5000, autoload: true, autoloadCallback: dbLoadCallback});
-var heroes;
+var skills, heroes;
 
 function dbLoadCallback() {
     heroes = db.getCollection('heroes');
+    skills = db.getCollection('skills');
 };
 
 /* GET home page. */
 router.post('/', function(req, res, next) {
-    res.send(heroes.data);
+    var data = heroes.data;
+    data.forEach((hero) => {
+        hero.skill = skills.findOne({'id': hero.skill});
+    });
+    res.send(data);
 });
 
 module.exports = router;
