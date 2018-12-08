a35a57d8a44fa94365600cfec36a79749ef3a4ad
IllyaVolkov <volkov18598>
2018-12-08 02:21:23 +0200

    fix getting heroes & skills

--- routes/heroes.js
+++ routes/heroes.js
@@ -2,11 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 var loki = require('lokijs');
-var db = new loki('mock.json', {autosave: true, autosaveInterval: 5000, autoload: true});
+var db = new loki('mock.json', {autosave: true, autosaveInterval: 5000, autoload: true, autoloadCallback: dbLoadCallback});
+var heroes;
+
+function dbLoadCallback() {
+    heroes = db.getCollection('heroes');
+};
 
 /* GET home page. */
 router.post('/', function(req, res, next) {
-    heroes = db.getCollection('heroes');
     res.send(heroes.data);
 });
 
--- routes/skills.js
+++ routes/skills.js
@@ -2,11 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 var loki = require('lokijs');
-var db = new loki('mock.json', {autosave: true, autosaveInterval: 5000, autoload: true});
+var db = new loki('mock.json', {autosave: true, autosaveInterval: 5000, autoload: true, autoloadCallback: dbLoadCallback});
+var skills;
+
+function dbLoadCallback() {
+    skills = db.getCollection('skills');
+};
 
 /* GET home page. */
 router.post('/', function(req, res, next) {
-    skills = db.getCollection('skills');
     res.send(skills.data);
 });
 
